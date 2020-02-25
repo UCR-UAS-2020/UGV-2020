3b256d3823ddf31280ce05d7cc63a0ced100785d
PikaSannnnn <Dong.Stephen11@gmail.com>
2020-02-24 17:18:46 -0800

    Added Motor Code

---
+++ UGV-Components/Motor.cpp
@@ -0,0 +1,16 @@
+//#include "Motor.h"
+//
+//void motorStop() {
+//  // analogWrite(RightEn, 0);
+//  // analogWrite(LeftEn, 0);
+//}
+//
+//void turnUGV(const double& locAngle, const double& UGV_Angle) {
+//    // if () {
+//    //
+//    //     delay(50);
+//    // } else if () {
+//    //
+//    //     delay(50);
+//    // }
+//}
---
+++ UGV-Components/Motor.h
@@ -0,0 +1,7 @@
+//#ifndef MOTOR_H
+//#define MOTOR_H
+//
+////void motorStop();
+////void turnUGV(const double& locAngle, const double& UGV_Angle);
+//
+//#endif
